a5712f14ef225c84cbd02598f2a86714107ece7a
Raindy <raindy_nightmare@foxmail.com>
2017-07-15 23:18:37 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by Raindy on 2017/7/13.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/example/coolweather/gson/Now.java
@@ -0,0 +1,23 @@
+package com.example.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Raindy on 2017/7/13.
+ */
+
+public class Now {
+
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More {
+
+        @SerializedName("txt")
+        public String info;
+
+    }
+}
